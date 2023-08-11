f5757bbe05873b457fdd80e77b61c6eb58336d71
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-08-11 22:20:49 +0800

    Successful pipeline

--- extract_data.py
+++ extract_data.py
@@ -6,39 +6,48 @@ import os
 import reverse_geocoder as rg
 from helpers import (EXCLUDE_MAX_LATITUDE, EXCLUDE_MAX_LONGITUDE,
                      MAX_LATITUDE_B, MAX_LONGITUDE_R, MIN_LATITUDE_T,
-                     MIN_LONGITUDE_L, get_coordinates, tuplemaker)
+                     MIN_LONGITUDE_L,
+                     get_coordinates_x, get_coordinates_y, tuplemaker, evaluate_dl_speed)
 
 
 def main():
-    parquet_path = "./parquet_files/"
-    csv_path = "./raw_csv_files/"
+    input_parquet_path = "./parquet_files/"
+    output_parquet_path = "./f_parquet_files/"
 
-    dir_list = os.listdir(parquet_path)
-    if not os.path.isdir(csv_path):
-        os.mkdir(csv_path)
+    dir_list = os.listdir(input_parquet_path)
+
+    if not os.path.isdir(output_parquet_path):
+        os.mkdir(output_parquet_path)
 
     for file in dir_list:
-        ph_df = convert_to_csv(parquet_path, file)
+        print(f"Processing {input_parquet_path + file}")
+        ph_df = extract_all_ph_coordinates(input_parquet_path, file)
         rg_df = reverse_geocode(ph_df)
-        # slice_to_chunks(csv_path, file)
+        refined_df = label_data_rows(rg_df, file)
+        refined_df.to_parquet(output_parquet_path + file)
 
 
-def convert_to_csv(path, file):
+def extract_all_ph_coordinates(path, file):
+
     target_path = path + file
-    output_path = "./raw_csv_files/"
-
     df = pd.read_parquet(target_path)
+    print(len(df))
 
     if 'tile_x' not in df.columns or 'tile_y' not in df.columns:
-        df[['tile_x', 'tile_y']] = df.apply(lambda row:
-                                            get_coordinates(row['tile']), axis=1)
 
+        df['tile_x'] = df.apply(lambda row:
+                                get_coordinates_x(row['tile']), axis=1, result_type='expand')
+
+        df['tile_y'] = df.apply(lambda row:
+                                get_coordinates_y(row['tile']), axis=1, result_type='expand')
+
+    '''First filter'''
     filt_1_df = df[(df['tile_y'] >= MIN_LATITUDE_T) &
                    (df['tile_y'] <= MAX_LATITUDE_B) &
                    (df['tile_x'] >= MIN_LONGITUDE_L) &
                    (df['tile_x'] <= MAX_LONGITUDE_R)]
 
-    # Raw PH
+    '''Second filter'''
     filt_2_df = filt_1_df[
         (filt_1_df['tile_y'] < EXCLUDE_MAX_LATITUDE) &
         (filt_1_df['tile_x'] < EXCLUDE_MAX_LONGITUDE)
@@ -49,37 +58,9 @@ def convert_to_csv(path, file):
 
     ph_df = result[result['_merge'] == 'left_only'].drop(columns=['_merge'])
 
-    # filename = file.split(".")[0]
-
-    # ph_df.to_csv(output_path + filename + ".csv", index=False)
-
     print(f"{len(ph_df)}")
 
     return ph_df
-
-
-# def slice_to_chunks(csv_path, file):
-#     output_path = csv_path + file.split(".")[0]
-
-#     if not os.path.isdir(output_path):
-#         os.mkdir(output_path)
-
-#     file = file.split(".")[0] + ".csv"
-
-#     df_iter = pd.read_csv(csv_path + file, iterator=True, chunksize=500)
-#     batch_no = 1
-
-#     while True:
-#         try:
-#             batch_df = next(df_iter)
-#             batch_df.to_csv(
-#                 f"{output_path}/batch_no_{batch_no}.csv", index=False)
-#             batch_no += 1
-#         except StopIteration:
-#             print(f"Done. Processed a total of {batch_no}")
-#             break
-
-#     print(f"{file} successfully sliced to chunks...")
 
 
 def reverse_geocode(df: pd.DataFrame):
@@ -105,11 +86,22 @@ def reverse_geocode(df: pd.DataFrame):
         chunk_df['admin_2'] = admin_2_list
 
         main_df = pd.concat([main_df, chunk_df], ignore_index=True)
+
         print(f"Added {start} to {start + chunksize}")
 
     print(f"{len(main_df)}")
-    main_df.to_csv("test.csv")
+    main_df.drop('input', axis=1, inplace=True)
+
     return main_df
+
+
+def label_data_rows(df: pd.DataFrame, file):
+
+    cover_date = file.split("_")[0]
+    df['dl_speed_level'] = pd.Timestamp(cover_date)
+    df['dl_speed_level'] = df.apply(
+        lambda row: evaluate_dl_speed(row['avg_d_kbps']), axis=1)
+    return df
 
 
 if __name__ == "__main__":
--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 from shapely.wkt import loads
+import pandas as pd
+
 MIN_LATITUDE_T = 4.90652903432457
 MAX_LATITUDE_B = 20.906529034
 MIN_LONGITUDE_L = 116.790348960255
@@ -9,19 +11,53 @@ EXCLUDE_MAX_LONGITUDE = 119.61264505265808
 
 
 def get_coordinates(wkt_polygon):
-
-    # Parse the WKT polygon
+    '''Parse the WKT polygon'''
     polygon = loads(wkt_polygon)
 
     # Extract the coordinates
     coordinates = list(polygon.exterior.coords)[0]
     longitude, latitude = coordinates
 
-    return longitude, latitude
+    return pd.Series({'tile_x': longitude, 'tile_y': latitude})
+
+
+def get_coordinates_x(wkt_polygon):
+    '''Parse the WKT polygon'''
+    polygon = loads(wkt_polygon)
+
+    # Extract the coordinates
+    coordinates = list(polygon.exterior.coords)[0]
+    longitude = coordinates[0]
+
+    return longitude
+
+
+def get_coordinates_y(wkt_polygon):
+    '''Parse the WKT polygon'''
+    polygon = loads(wkt_polygon)
+
+    # Extract the coordinates
+    coordinates = list(polygon.exterior.coords)[0]
+    latitude = coordinates[1]
+
+    return latitude
 
 
 def tuplemaker(lat, long):
     return lat, long
+
+
+def evaluate_dl_speed(dl):
+    if dl > 45000.00:
+        return "45MB/s +"
+    elif dl > 35000.00:
+        return "35MB/s to 45MB/s"
+    elif dl > 25000.00:
+        return "25MB/s to 35MB/s"
+    elif dl > 10000.00:
+        return "10MB/s to 25MB/s"
+    else:
+        return "< 10MB/s"
 
 
 def log_update(target_path, folder, csv_input):
